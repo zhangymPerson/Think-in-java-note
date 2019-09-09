eaf81a5b1830f01cd7585dd9f7c5f0f8c6288a3f
zhangyanmingjiayou@163.com <zhangyanmingjiayou@163.com>
2019-09-09 23:16:57 +0800

    add class

---
+++ javacode/bookcode/ThinkInJavaNote/src/main/java/cn/danao/code/eighteen/DirFilter.java
@@ -0,0 +1,20 @@
+package cn.danao.code.eighteen;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * @author <a href="mailto:zhangyanmingjiayoiu@163.com">Thinkpad</a>
+ * @version 1.0
+ * @classname DirFilter
+ * @descriptionclass 1.****************
+ * 2.****************
+ * @createdate 2019/8/18 9:58
+ * @since 1.0
+ */
+public class DirFilter implements FilenameFilter {
+    @Override
+    public boolean accept(File dir, String name) {
+        return false;
+    }
+}
---
+++ javacode/bookcode/ThinkInJavaNote/src/main/java/cn/danao/code/eighteen/DirList.java
@@ -0,0 +1,19 @@
+package cn.danao.code.eighteen;
+
+import java.io.File;
+
+/**
+ * @author <a href="mailto:zhangyanmingjiayoiu@163.com">Thinkpad</a>
+ * @version 1.0
+ * @classname DirList
+ * @descriptionclass 1.****************
+ * 2.****************
+ * @createdate 2019/8/18 9:57
+ * @since 1.0
+ */
+public class DirList {
+    public static void main(String[] args) {
+        File file = new File(".");
+
+    }
+}
