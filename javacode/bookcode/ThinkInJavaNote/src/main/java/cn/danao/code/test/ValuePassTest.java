f073fbb6eb2eedd785f49ec594e070591a262030
zhangyanmingjiayou@163.com <zhangyanmingjiayou@163.com>
2019-09-13 09:05:01 +0800

    alter utf-8

--- javacode/bookcode/ThinkInJavaNote/src/main/java/cn/danao/code/test/ValuePassTest.java
+++ javacode/bookcode/ThinkInJavaNote/src/main/java/cn/danao/code/test/ValuePassTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
  * @author <a href="mailto:zhangyanmingjiayoiu@163.com">Thinkpad</a>
  * @version 1.0
  * @classname ValuePassTest
- * @descriptionclass 1.值传递测试
+ * @descriptionclass 1.鍊间紶閫掓祴璇�
  * 2.****************
  * @createdate 2019/9/13 8:37
  * @since 1.0
@@ -17,24 +17,24 @@ public class ValuePassTest {
     public String name;
 
     public String test(String str) {
-        System.out.println("args" + str);
+        System.out.println("args = " + str);
         str = str + "test method";
-        System.out.println("alter args" + str);
+        System.out.println("alter args= " + str);
         return str;
     }
 
 
     public int testInt(int num) {
-        System.out.println("args" + num);
+        System.out.println("args = " + num);
         num = num + 1;
-        System.out.println("alter args" + num);
+        System.out.println("alter args = " + num);
         return num;
     }
 
     public String testInts(int num) {
-        System.out.println("args" + num);
+        System.out.println("args = " + num);
         num = num + 1;
-        System.out.println("alter args" + num);
+        System.out.println("alter args = " + num);
         return String.valueOf(num);
     }
 
