fa782ef15f904bed91aa6885b11e242646024fae
zhangym-aliyun <zhangyanmingjiayou@163.com>
2020-01-09 11:30:46 +0800

    add go code

--- bookcode/goproject/base/main/helloword.go
+++ bookcode/goproject/base/main/helloword.go
@@ -4,8 +4,9 @@ package main
 
 // 导入其他包
 import (
-    "fmt"
-    log "github.com/cihub/seelog"
+	"fmt"
+	log "github.com/cihub/seelog"
+	"runtime"
 )
 
 // 常量定义
@@ -23,9 +24,19 @@ type gopher struct{}
 // 接口的声明
 type golang interface{}
 
+//函数 a b, return a+b
+func Sum(a, b int) int { return a + b }
+
+
 // 由main函数作为程序入口点启动
 func main() {
-    fmt.Println("Hello World!")
-    defer log.Flush()
+	fmt.Println("Hello World!")
+	defer log.Flush()
     log.Info("Hello from Seelog!")
+    //定义变量
+    var a int = 2
+    var b int = 3
+    log.Info(Sum(a,b))
+	//go版本
+	log.Info(runtime.Version())
 }
