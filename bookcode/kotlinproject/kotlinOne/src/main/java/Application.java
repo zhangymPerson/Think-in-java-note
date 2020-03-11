ee452c82d83181adfc2413400a4689cefc1c8c3a
zhangyanmingjiayou@163.com <zhangyanmingjiayou@163.com>
2020-03-11 22:49:03 +0800

    add kotlin project

---
+++ bookcode/kotlinproject/kotlinOne/src/main/java/Application.java
@@ -0,0 +1,13 @@
+/**
+ * date 2020/3/11 22:22 <br/>
+ * descriptionclass <br/>
+ *
+ * @author danao
+ * @version 1.0
+ * @since 1.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
---
+++ bookcode/kotlinproject/kotlinOne/src/main/kotlin/HelloWord.kt
@@ -0,0 +1,10 @@
+
+//kotlin的 hello word
+fun main(args:Array<String>){
+    println("Hello Word!")
+}
+
+//kotlin基本语法
+fun learning(){
+
+}
