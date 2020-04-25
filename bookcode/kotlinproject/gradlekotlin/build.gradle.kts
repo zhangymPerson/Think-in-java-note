506c29618c9a8b9c61f9bb351c75cced11ae3811
zhangyanmingjiayou@163.com <zhangyanmingjiayou@163.com>
2020-04-25 17:57:37 +0800

    新增 编写稍微复杂的任务

--- bookcode/kotlinproject/gradlekotlin/build.gradle.kts
+++ bookcode/kotlinproject/gradlekotlin/build.gradle.kts
@@ -6,12 +6,64 @@
 
 //task案例
 
+
+// task 扫描时运行/运行时运行两种
+//构建是执行
 task("taskname", {
     //任务内容
     //构建时会先扫描并执行所有task内容
     println("test task")
 })
 
+//doFirst{} 在运行任务时才会执行当前脚本
 task("runtask", {
     doFirst { print("运行时才执行的task,扫描的时候不执行") }
+})
+
+//任务依赖
+
+// a - > b -> c 执行顺序
+
+//a
+task("a", {
+    doFirst {
+        println("one : a task")
+    }
+})
+
+//b
+task("b", {
+    doFirst {
+        println("two : b task")
+    }
+    //dependsOn(taskname) 通过调用这个函数来实现任务的依赖关系
+}).dependsOn("a")
+
+//c
+task("c", {
+    doFirst {
+        println("three : c task")
+    }
+}).dependsOn("b")
+
+
+//增量式更新
+//插件 常用
+// application 编译应用程序 java/kotlin
+// java 编译Java程序
+// war web项目
+plugins {
+    java
+}
+
+//获取文件名
+task("filename", {
+    var src = fileTree("src")
+    var info = file("file.log")
+    src.forEach {
+        if (it.isFile) {
+            info.appendText(it.absolutePath)
+            info.appendText("\n")
+        }
+    }
 })
---
+++ bookcode/kotlinproject/gradlekotlin/src/main/java/cn/danao/App.java
@@ -0,0 +1,15 @@
+﻿package cn.danao;
+
+/**
+ * date 2020/4/25 17:46 <br/>
+ * descriptionclass <br/>
+ *
+ * @author danao
+ * @version 1.0
+ * @since 1.0
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
