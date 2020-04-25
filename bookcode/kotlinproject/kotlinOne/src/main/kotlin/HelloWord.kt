904d3b49f934726df7f26633035106d5bcf23c6f
zhangyanmingjiayou@163.com <zhangyanmingjiayou@163.com>
2020-04-25 16:02:49 +0800

    添加 gradle测试代码

--- bookcode/kotlinproject/kotlinOne/src/main/kotlin/HelloWord.kt
+++ bookcode/kotlinproject/kotlinOne/src/main/kotlin/HelloWord.kt
@@ -1,10 +1,11 @@
-
 //kotlin的 hello word
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
     println("Hello Word!")
+    var test = "测试"
+    learning(test)
 }
 
 //kotlin基本语法
-fun learning(){
-
+fun learning(str: String) {
+    print(str)
 }
