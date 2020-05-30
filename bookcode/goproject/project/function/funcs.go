1ddb62be8ac7d6eb8043668604f1129ce9bf889f
zhangyanmingjiayou@163.com <zhangyanmingjiayou@163.com>
2020-05-30 16:33:55 +0800

    使用 goland美化代码

--- bookcode/goproject/project/function/funcs.go
+++ bookcode/goproject/project/function/funcs.go
@@ -5,20 +5,21 @@ import "fmt"
 //函数作为变量是第一等公民
 //定义一个函数类型
 type function func(int) int
+
 //定义函数变量
 var f function
 
-func FuncUse(){
-	f = func(a int) int{
+func FuncUse() {
+	f = func(a int) int {
 		return a
 	}
-	
+
 	res := f(12)
 
-	fmt.Printf("res = %v\n",res)
+	fmt.Printf("res = %v\n", res)
 
-	f = func(a int) int{
-		return a+a
+	f = func(a int) int {
+		return a + a
 	}
 
 	f(11)
--- bookcode/goproject/project/model/TreeNode.go
+++ bookcode/goproject/project/model/TreeNode.go
@@ -4,20 +4,17 @@ import (
 	"fmt"
 )
 
-
-
-type TreeNode struct{
-	Value int
-	Left,Right *TreeNode
+type TreeNode struct {
+	Value       int
+	Left, Right *TreeNode
 }
 
 /**
 * 指针接收者
-*/
+ */
 func (node *TreeNode) Print() {
-	if node == nil{
+	if node == nil {
 		fmt.Print("node = %s 是个nil")
 	}
-	fmt.Printf("node value = %v,left = %v.right = %v",node.Value,node.Left,node.Right)
+	fmt.Printf("node value = %v,left = %v.right = %v", node.Value, node.Left, node.Right)
 }
-
