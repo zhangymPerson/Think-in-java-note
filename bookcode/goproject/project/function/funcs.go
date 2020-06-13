d99089808fcaea10dd4c00c38fc737fd4bec5968
zhangym <zhangyanmingjiayou@163.com>
2020-06-13 17:05:51 +0800

    add go code

--- bookcode/goproject/project/function/funcs.go
+++ bookcode/goproject/project/function/funcs.go
@@ -23,3 +23,26 @@ func FuncUse(){
 
 	f(11)
 }
+
+//定义一个Person对象
+type Person struct{
+	//
+	Id int
+	Sex string
+	Name string
+	Age int
+	//其他信息
+	info map[string]string
+}
+
+func (p  *Person) Print() {
+	fmt.Printf("个人信息：id = %v,性别 = %v,姓名 = %v,年龄 = %v",p.Id,p.Sex,p.Name,p.Age)
+}
+
+//匿名函数
+func (p *Person) anonymityFunc(){
+	res := func (a int,b int) int{
+		return a+b
+	}(10,11)
+	fmt.Printf("p = %v,res = %v\n",p,res)
+}
--- bookcode/goproject/project/function/funcs_test.go
+++ bookcode/goproject/project/function/funcs_test.go
@@ -2,4 +2,12 @@ package function
 import "testing"
 func TestFuncUse(t *testing.T) {
     t.Log("hello world")
+    p :=Person{
+        Id : 1,
+        Age:12,
+        Name:"张三",
+        Sex:"男",
+    }
+    p.anonymityFunc()
+    p.Print()
 }
