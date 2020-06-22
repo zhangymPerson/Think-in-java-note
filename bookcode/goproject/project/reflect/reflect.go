0ee329a95143de3d9fa6b636bec334c4c7ae1a57
zhangym <zhangyanmingjiayou@163.com>
2020-06-22 16:36:55 +0800

    添加 go反射将interface转成 []interface

---
+++ bookcode/goproject/project/reflect/reflect.go
@@ -0,0 +1,24 @@
+package reflect
+
+import (
+	"log"
+	"reflect"
+)
+
+//将 interface{} 对象转成数组
+//需要使用到反射知识
+func InterfaceToSlice(obj interface{}) []interface{} {
+	//获取反射后的Value对象
+	v := reflect.ValueOf(obj)
+	t := reflect.TypeOf(obj)
+	log.Println("反射后 vale = ", v.Kind(), "type = ", t)
+	if v.Kind() != reflect.Slice {
+		panic("toslice arr not slice")
+	}
+	l := v.Len()
+	res := make([]interface{}, l)
+	for i := 0; i < l; i++ {
+		res[i] = v.Index(i).Interface()
+	}
+	return res
+}
