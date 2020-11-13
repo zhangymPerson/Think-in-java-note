996b84cfd76c7ebcb32c99739491993112bd61c1
zhangym <zhangyanmingjiayou@163.com>
2020-11-13 11:44:01 +0800

    add log4j and test

--- bookcode/kotlinproject/kotlin-code/src/main/java/cn/danao/main/App.java
+++ bookcode/kotlinproject/kotlin-code/src/main/java/cn/danao/main/App.java
@@ -1,5 +1,10 @@
 package cn.danao.main;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+
 /**
  * date 2020/7/19 12:20 <br/>
  * description class <br/>
@@ -8,8 +13,19 @@ package cn.danao.main;
  * @version 1.0
  * @since 1.0
  */
+@Slf4j
 public class App {
     public static void main(String[] args) {
         System.out.println("java main() -  混编代码");
+
+    }
+
+    @Test
+    public void test() {
+        run("");
+    }
+
+    public void run(@NotNull String str) {
+        log.info("test");
     }
 }
