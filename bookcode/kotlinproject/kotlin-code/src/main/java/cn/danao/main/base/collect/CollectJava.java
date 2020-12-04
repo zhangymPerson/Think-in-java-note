14d8ed2a914add169cafab1f252e8f8ab5a8b7cb
zhangym <zhangyanmingjiayou@163.com>
2020-12-04 15:07:53 +0800

    add code

--- bookcode/kotlinproject/kotlin-code/src/main/java/cn/danao/main/base/collect/CollectJava.java
+++ bookcode/kotlinproject/kotlin-code/src/main/java/cn/danao/main/base/collect/CollectJava.java
@@ -1,10 +1,22 @@
-package cn.danao.main.base.collect;/**
-* date 2020/11/26 16:02 <br/>
-* description class <br/>
-*
-* @author danao 
-* @version 1.0
-* @since 1.0
-*/
+package cn.danao.main.base.collect;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * date 2020/11/26 16:02 <br/>
+ * description class <br/>
+ *
+ * @author danao
+ * @version 1.0
+ * @since 1.0
+ */
 public class CollectJava {
+
+    public void test() {
+        List<String> list = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+    }
 }
--- bookcode/kotlinproject/kotlin-code/src/main/java/cn/danao/main/base/collect/CollectKotlin.kt
+++ bookcode/kotlinproject/kotlin-code/src/main/java/cn/danao/main/base/collect/CollectKotlin.kt
@@ -1,4 +1,13 @@
 package cn.danao.main.base.collect
 
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.List
+import kotlin.collections.Map
+
 class CollectKotlin {
+    fun test() {
+        val list: List<String> = ArrayList()
+        val map: Map<String, String> = HashMap()
+    }
 }
