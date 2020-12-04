ba330a3d38f72a28a33e28a7f866681079fdd6e5
zhangym <zhangyanmingjiayou@163.com>
2020-12-04 19:28:44 +0800

    go 运行测试

--- bookcode/goproject/one/main.go
+++ bookcode/goproject/one/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
+
 	"./funcs"
 )
 
-
 //函数
 
-func main(){
+func main() {
 	fmt.Print("测试")
+	// test()
+	t := time.Now()
+	n := 1000000
+	StrAppend(n)
+	elapsed := time.Since(t)
+	fmt.Println("app elapsed:", elapsed)
+}
+
+func test() {
 	//这个是私有函数
 	// funcs.privateFunc()
 	funcs.PublicFunc()
@@ -19,17 +30,27 @@ func main(){
 	funcs.TestStructTwo()
 	fmt.Println("----------------------------------")
 	var p funcs.Person
-	p.Name="测试"
+	p.Name = "测试"
 	a := 23
 	p.Age = &a
 	//定义切片
-	ints :=[]int{1,2,3}
+	ints := []int{1, 2, 3}
 	//集合
 	maps := make(map[string]string)
-	maps["key"]="value"
-	maps["key1"]="value1"
-	maps["key2"]="value2"
-	p.Maps=maps
+	maps["key"] = "value"
+	maps["key1"] = "value1"
+	maps["key2"] = "value2"
+	p.Maps = maps
 	p.Slice = ints
 	fmt.Print(p)
+}
+
+//StrAppend is 字符串拼接测试
+func StrAppend(n int) {
+	var a = "a"
+	for i := 0; i < n; i++ {
+		a = a + strconv.Itoa(i)
+	}
+	fmt.Printf("a的长度%v\n", len(a))
+	// fmt.Printf("a=%v\na的长度%v\n", a, len(a))
 }
