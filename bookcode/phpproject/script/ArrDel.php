d37afd052c395b1b5db58adb5bb6205ca314c36a
zhangyanming <zhangyanmingjiayou@163.com>
2021-08-10 10:14:59 +0800

    add php code

---
+++ bookcode/phpproject/script/ArrDel.php
@@ -0,0 +1,19 @@
+<?php
+
+$arr = array('1', '2', '3');
+$brr = array(
+    'a' => '1',
+    'b' => '2',
+    'c' => '3',
+    'd' => array("aaa" => "ccc", "bbb" => "ddd"),
+);
+
+foreach ($brr as $key => &$value) {
+    var_dump($key . '=' . $value);
+    if($value == "2") {
+        unset($brr[$key]);
+    }
+    $value['a'] = "b";
+//    $arrOne['a'] = "b";
+}
+echo json_encode($brr);
