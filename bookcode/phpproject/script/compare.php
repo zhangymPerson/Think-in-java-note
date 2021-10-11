0a095552a65f94dffe772cbb9e974f22f879aa44
zhangyanming <zhangyanmingjiayou@163.com>
2021-10-11 11:55:42 +0800

    add php string script

--- bookcode/phpproject/script/compare.php
+++ bookcode/phpproject/script/compare.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * 字符串为空判断
+ */
 class CompareTest {
     public function __construct() {
     }
@@ -15,11 +18,24 @@ class CompareTest {
         echo $a == $b . PHP_EOL;
         if ($a == $b) {
             echo "\$a = \$b";
-        }else{
+        } else {
             echo "$a != $b";
+        }
+    }
+
+    public function isEmpty(string $var = null) {
+        # code...
+        if (empty($var)) {
+            echo "$var 为空" . PHP_EOL;
+        } else {
+            echo "$var 不为空" . PHP_EOL;
         }
     }
 }
 
 $obj = new CompareTest();
 $obj->compareStr();
+$obj->isEmpty("a");
+$obj->isEmpty();
+$obj->isEmpty("");
+$obj->isEmpty("  ");
